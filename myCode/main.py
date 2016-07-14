928ab1db9c298ba6a371d51848d7170773febb26
churq <churq@hotmail.com>
2016-07-15 09:10:01 +1000

    refactor the code

--- myCode/main.py
+++ myCode/main.py
@@ -1,43 +1,19 @@
 import argparse
-import re
-from operator import itemgetter
 from myLib import *
-
-def run(sp,ep):
-    if analysis(sp,ep):
-        k = knight()
-        result = [sp]
-        results = []
-        all_knight_path(k, sp, ep, result, results)
-        if results:
-            ordered_result = []
-            for r in results:
-                temp = [chr(i[0] + 96) + str(i[1]) for i in r]
-                ordered_result.append(('-'.join(temp),''.join([i[0] for i in temp]),''.join([i[1] for i in temp])))
-            ordered_result = sorted(ordered_result,key = itemgetter(1,2))
-            for i in ordered_result:
-                print (i[0])
-
-        else:
-            print ([])
-    else:
-        print('illegal position, please make sure both values are in the range of 1 and 8')
-        return
 
 
 def main():
     parser = argparse.ArgumentParser(description='predict knight path')
     parser.add_argument('--start',type = str, required = True, help='start position of knight, make sure first letter second numeric')
     parser.add_argument('--end', type =str, required = True, help = 'end position of knight, make sure first letter second numeric')
+
     args = parser.parse_args()
+
     start_position = args.start
     end_position = args.end
-    if len(start_position)==2 and len(end_position) ==2 and re.match(r'[a-z][0-9]',start_position) and re.match(r'[a-z][0-9]',start_position):
-        s = (ord(start_position[0])-96,int(start_position[1]))
-        e = (ord(end_position[0])-96,int(end_position[1]))
-        run(s,e)
-    else:
-        print ('input format is not correct, please follows the format:[a-z][0-9]')
+
+    knight = Knight()
+    knight.jump(start_position,end_position)
 
 if __name__ =='__main__':
     main()
--- myCode/myLib.py
+++ myCode/myLib.py
@@ -3,79 +3,135 @@ all_knight_path: given start and end positions, calculate all the paths
 knight: instance of knight class has jump() method, which calculate
 all the next positions jumped from current position
 """
-class knight():
+import re
+from operator import itemgetter
+
+
+class ChessPiece():
     def __init__(self):
-        # all 8 possible positions
-        self.rules = {'m1':(2,1),'m2':(1,2),'m3':(-1,2),'m4':(-2,1),'m5':(-2,-1),'m6':(-1,-2),'m7':(1,-2),'m8':(2,-1)}
-        self.range = [8,8]
+        self.boardrange = 8
+        self.rules = {}
 
-    def _isvalid(self,x,y):
-        if (x>=1 and x<= self.range[0]) and (y>=1 and y <= self.range[1]):
-            return True
+    def _isvalid(self, x, y):
+        inRange = lambda x : 1 <= x <= self.boardrange
+        return inRange(x) and inRange(y)
+
+    def _num_to_letter (self, num):
+        return chr(num + 96)
+
+    def _letter_to_num(self, letter):
+        return ord(letter)-96
+    
+    def jump(self,start_position,end_position):
+        if self.rules:
+            if self._analysis(start_position,end_position):
+                result = [start_position]
+                results = []
+                self._paths_start_to_end(start_position, end_position, result, results)
+                if results:
+                    ordered_result = []
+                    for r in results:
+                        temp = [self._num_to_letter(i[0]) + str(i[1]) for i in r]
+                        ordered_result.append(
+                            ('-'.join(temp), ''.join([i[0] for i in temp]), ''.join([i[1] for i in temp])))
+                    ordered_result = sorted(ordered_result, key=itemgetter(1, 2))
+                    for i in ordered_result:
+                        print(i[0])
+
+                else:
+                    print([])
+
+                return results
+            else:
+                print ("input argument is invalid, please follow the format: [a-f][0-9]")
+                return None
+
         else:
-            return False
-    def _map(self,n):
-        return chr(n+96)
-
-    def jump(self,position):
-        try:
-            x = position[0]
-            y = position[1]
-        except:
-            print ("please make sure the input argument is the tuple/list of two elements (x,y)")
-            return
-        all_position = []
-        for i in self.rules.values():
-            if self._isvalid(x+i[0],y+i[1]):
-                all_position.append((x+i[0],y+i[1]))
-
-        if all_position:
-            #end_position = '-'.join(['{}{}'.format(self._map(p[0]),str(p[1])) for p in all_position ])
-            #end_position = ['{}{}'.format(self._map(p[0]),str(p[1])) for p in all_position ]
-            return all_position
-        else:
+            print ("no rule is defined")
             return None
 
 
-def analysis(startp,endp):
-    """
-    check if start and end position are in the range of board
-    return: True or False
-    startp: e.g (1,1)
-    endp: e.g (4,4)
-    """
 
-    if not (startp[0] in [1,2,3,4,5,6,7,8] and startp[1] in [1,2,3,4,5,6,7,8] and endp[0] in [1,2,3,4,5,6,7,8] and endp[1] in [1,2,3,4,5,6,7,8]):
+    def next_move(self, current_position):
+        try:
+            x = current_position[0]
+            y = current_position[1]
+        except:
+            print("please make sure the input argument is the tuple/list of two elements (x,y)")
+            return
+        next_move_position = []
+        for i in self.rules.values():
+            if self._isvalid(x + i[0], y + i[1]):
+                next_move_position.append((x + i[0], y + i[1]))
 
-        return False
-    else:
-        return True
+        if next_move_position:
 
+            return next_move_position
+        else:
+            return None
 
-def all_knight_path(k,sp,ep,result,results):
+    def _analysis(self,start_position, end_position):
         """
-        k:knight object
-        sp: start position
-        ep: end position
+        check if start and end position are in the range of board
+        return: True or False
+        startp: e.g (1,1)
+        endp: e.g (4,4)
+        """
+        if len(start_position) == 2 and len(end_position) == 2 and re.match(r'[a-z][0-9]', start_position) and re.match(
+                r'[a-z][0-9]', end_position):
+            start_position = (ord(start_position[0]) - 96, int(start_position[1]))
+            end_position = (ord(end_position[0]) - 96, int(end_position[1]))
+
+            return self._isvalid(start_position,end_position)
+        else:
+            return False
+
+    def _paths_start_to_end(self, start_position, end_position, result, results):
+        """
+        start position:(1,1)
+        end position
         result: parent node, initial value is [sp]
         results: reference of empty list which is going to store all the paths
         """
-        candidatePos = k.jump(sp)
+        candidatePos = self.next_move(start_position)
         if not result:
             return None
 
         try:
             for i in candidatePos:
-                if len(result)>=5:
+                if len(result) >= 5:
                     return result[:-1]
                 if i in result:
                     continue
-                if i ==ep:
-                    results.append(result+[i])
+                if i == end_position:
+                    results.append(result + [i])
                     continue
                 result.append(i)
-                result = all_knight_path(k,i,ep,result,results)
+                result = self._paths_start_to_end(i, end_position, result, results)
             return result[:-1]
         except:
             return None
+
+class Knight():
+    def __init__(self):
+        # all 8 possible positions
+        self.rules = {
+            'm1':(2,1),
+            'm2':(1,2),
+            'm3':(-1,2),
+            'm4':(-2,1),
+            'm5':(-2,-1),
+            'm6':(-1,-2),
+            'm7':(1,-2),
+            'm8':(2,-1)
+        }
+
+
+
+
+
+
+
+
+
 
